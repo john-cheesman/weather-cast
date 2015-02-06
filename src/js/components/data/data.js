595e6ef732a5b8fd2cda56a88918b7ee870b62bd
John Cheesman <info@johncheesman.org.uk>
2015-02-06 11:45:44 +0000

    Reformat data, add wind and humidity

--- src/js/components/data/data.js
+++ src/js/components/data/data.js
@@ -9,7 +9,7 @@
 
     function dataSource($resource) {
         return $resource(
-            'http://api.openweathermap.org/data/2.5/forecast/daily?q=derby,uk&cnt=7',
+            'http://api.openweathermap.org/data/2.5/forecast/daily?q=:location,uk&cnt=7',
             {
                 location: '@location'
             },
