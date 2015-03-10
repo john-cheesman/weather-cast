502a6b17ba72f64805188d1ed48c62413736b1dc
John Cheesman <info@johncheesman.org.uk>
2015-03-10 06:06:35 +0000

    Add basic styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-var paths, gulp, sequence, del, concat, uglify;
+var paths, gulp, sequence, del,
+    concat, uglify, sass, autoprefixer,
+    minifycss;
 
-gulp     = require('gulp');
-sequence = require('run-sequence');
-del      = require('del');
-concat   = require('gulp-concat');
-uglify   = require('gulp-uglify');
+gulp         = require('gulp');
+sequence     = require('run-sequence');
+del          = require('del');
+concat       = require('gulp-concat');
+uglify       = require('gulp-uglify');
+sass         = require('gulp-ruby-sass');
+autoprefixer = require('gulp-autoprefixer');
+minifycss    = require('gulp-minify-css');
 
 paths = {
     scripts: {
@@ -16,6 +21,10 @@ paths = {
         ],
         dest: 'web/js'
     },
+    styles: {
+        src: 'src/scss/**/*.scss',
+        dest: 'web/css'
+    },
     images: {
         src: 'src/images/*',
         dest: 'web/images'
@@ -25,8 +34,16 @@ paths = {
 gulp.task('scripts', function() {
     gulp.src(paths.scripts.src)
         .pipe(concat('scripts.js'))
-        //.pipe(uglify())
+        .pipe(uglify())
         .pipe(gulp.dest(paths.scripts.dest));
+});
+
+gulp.task('styles', function() {
+    gulp.src(paths.styles.src)
+        .pipe(sass())
+        .pipe(minifycss())
+        //.pipe(autoprefixer(['last 2 versions']))
+        .pipe(gulp.dest(paths.styles.dest));
 });
 
 gulp.task('images', function() {
@@ -37,16 +54,18 @@ gulp.task('images', function() {
 gulp.task('clean', function() {
    del([
        paths.scripts.dest,
+       paths.styles.dest,
        paths.images.dest
    ]);
 });
 
 gulp.task('build', function() {
-    sequence('clean', 'scripts', 'images');
+    sequence('clean', 'scripts', 'styles', 'images');
 });
 
 gulp.task('watch', function() {
     gulp.watch(paths.scripts.src, ['scripts']);
+    gulp.watch(paths.styles.src, ['styles']);
 });
 
 gulp.task('default', function() {
